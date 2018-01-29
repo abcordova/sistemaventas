164fdb987a4b38fa3ab92e135e264e6d5551261b
rabileon <rabi_leon@hotmail.com>
2018-01-28 21:01:02 -0600

    global.php
    Se agrega las variables globales

---
+++ config/global.php
@@ -0,0 +1,20 @@
+<?php
+//ip de la pc servidor de base de datos
+define("DB_HOST", "localhost");
+
+//Nombre de la base de datos
+define("DB_NAME", "dbsistemaventas");
+
+//Usuario de la base de datos
+define("DB_USER", "root");
+
+//Contraseña del usuatio de la base de datos
+define("DB_PASSWORD", "");
+
+//Definimos la codificación de los caracteres
+define("DB_ENCODE", "utf8");
+
+//Definimos una constante como nombre del proyecto
+define("PRO_NOMBRE","RadixVentas");
+
+?>
